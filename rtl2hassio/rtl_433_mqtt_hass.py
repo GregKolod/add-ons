ea67a4760d48822a19ebc9854e44656c2d8d2784
GregKolod <40234158+GregKolod@users.noreply.github.com>
2020-07-20 16:32:49 +0200

    first commit

--- rtl2hassio/rtl_433_mqtt_hass.py
+++ rtl2hassio/rtl_433_mqtt_hass.py
@@ -327,8 +327,8 @@ def bridge_event_to_hass(mqttc, topic, data):
 
     if "channel" in data:
         channel = str(data["channel"])
-    # else:
-    #     channel = '4'
+    else:
+        channel = '4'
     print("channel", channel)
     # detect known attributes
     for key in data.keys():
