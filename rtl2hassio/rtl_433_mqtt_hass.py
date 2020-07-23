7e4b415c88929fe5c02e4f86eb7a152d452301e5
GregKolod <40234158+GregKolod@users.noreply.github.com>
2020-07-23 13:06:07 +0200

    first commit

--- rtl2hassio/rtl_433_mqtt_hass.py
+++ rtl2hassio/rtl_433_mqtt_hass.py
@@ -59,7 +59,7 @@ mappings = {
         "config": {
             "device_class": "window",
             "name": "command status 3",
-            "value_template": "{% if value_json.tristate[-2:] == '01'  %} '0' {% else %} off {% endif %}",
+            "value_template": "{% if value_json.tristate[-2:] == '01'  %} '0' {% else %} '1' {% endif %}",
             "payload_on": "0",
             "payload_off": "1"
 
