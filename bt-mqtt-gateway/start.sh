cee8734fd985f63135d8f8ed284d04e9cf0caf93
GregKolod <40234158+GregKolod@users.noreply.github.com>
2022-09-08 15:38:03 +0200

    Update start.sh

--- bt-mqtt-gateway/start.sh
+++ bt-mqtt-gateway/start.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/with-contenv bashio
 
-if ! [ -f '/application/config.yaml' ]; then
-    echo "There is no config.yaml! Check example config: https://github.com/zewelor/bt-mqtt-gateway/blob/master/config.yaml.example"
-    exit 1
-fi
-
-if [ "$DEBUG" = 'true' ]; then
-    echo "Start in debug mode"
-    python3 ./gateway.py -d
-    status=$?
-    echo "Gateway died..."
-    exit $status
-else
-    echo "Start in normal mode"
-    python3 ./gateway.py
-fi
+echo "Hello world!"
